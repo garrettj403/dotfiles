e1775c8aa72abd9ac1ff40738385f3b4e34b30f1
John Garrett <john.garrett@astro.ox.ac.uk>
2017-05-24 18:17:08 +0100

    Update

--- scripts/fmt_pwd.py
+++ scripts/fmt_pwd.py
@@ -1,13 +1,15 @@
+# Format the current working directory to use in the terminal prompt
+#
 # Based on:
 # https://askubuntu.com/questions/17723/trim-the-terminal-command-prompt-working-directory
 
 import os
+
 cwd = os.getcwd()
-homedir = os.path.expanduser('~')
-cwd = cwd.replace(homedir, '~', 1)
+home = os.path.expanduser('~')
+cwd = cwd.replace(home, '~', 1)
 cwd_split = cwd.split('/')
 if len(cwd_split) > 4:
     cwd = '.../' + '/'.join(cwd_split[-3:])
-# if len(pwd) > 33:
-#     pwd = pwd[:10]+'...'+pwd[-20:] # first 10 chars+last 20 chars
-print '%s' % cwd
+print(cwd)
+
